9db748e9ec89dfa76fcb3a05f150d93a05d9d405
edadeniz <e.deniz.celik@gmail.com>
2025-02-04 18:43:18 +0300

    Adding str class to the models

--- production/models.py
+++ production/models.py
@@ -6,20 +6,31 @@ class PartType(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class AircraftType(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
 
 class TeamType(models.Model):
     name = models.CharField(max_length=100, unique=True)
     managed_part_type = models.ForeignKey(PartType, null=True, blank=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return self.name
+
 
 class Staff(models.Model):
     name = models.CharField(max_length=100)
     team = models.ForeignKey(TeamType, related_name='staff', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Part(models.Model):
@@ -32,6 +43,9 @@ class Part(models.Model):
     def clean(self):
         if self.produced_by.managed_part_type != self.type:
             raise ValidationError('Team can only produce their assigned part type')
+
+    def __str__(self):
+        return f' {self.type.name} - {self.aircraft_type.name}'
 
 
 class Aircraft(models.Model):
@@ -46,3 +60,6 @@ class Aircraft(models.Model):
                 raise ValidationError(f'Incompatible part type')
             if part.is_used:
                 raise ValidationError('Cannot use already used parts')
+
+    def __str__(self):
+        return f' {self.type.name} Aircraft'
